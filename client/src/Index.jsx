7469f3e4820a00b0066006debe9bf41eb193eb96
Grant Belcher <gtbelch@gmail.com>
2020-03-19 01:16:19 -0700

    renders Sort Component to DOM, dropdown menu is no-longer transparent

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -2,5 +2,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-
+import Sort from './components/Sort.jsx';
 ReactDOM.render(<App />, document.getElementById('App'));
