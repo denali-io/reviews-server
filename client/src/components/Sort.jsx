463491f19e46cd1d1ff469931a235a06f455e353
Grant Belcher <gtbelch@gmail.com>
2020-03-19 00:15:36 -0700

    creates static dropdown menu with styling

--- client/src/components/Sort.jsx
+++ client/src/components/Sort.jsx
@@ -7,20 +7,54 @@ class Sort extends React.Component {
   constructor() {
     super();
     this.state = {
+      showMenu: false,
       value: 'Newest First',
     };
     this.menuClick = this.menuClick.bind(this);
   }
-  menuClick() {
-      console.log('clicked')
+
+  menuClick(event) {
+    event.preventDefault();
+    if (this.state.showMenu === true) {
+      this.setState({ showMenu: false });
+    } else {
+      this.setState({ showMenu: true });
+    }
   }
 
   render() {
     return (
-      <span className="sortMenu" onClick={this.menuClick} >
-        Sort by
-        <strong>{` ${this.state.value} `}</strong>
-        <FontAwesomeIcon icon={faSortDown} size="xs" />
+      <span>
+        <div className="container">
+          <button type="button" className="button" onClick={this.menuClick}>
+            Sort by
+            <strong>{` ${this.state.value} `}</strong>
+            <FontAwesomeIcon icon={faSortDown} size="xs" />
+          </button>
+          <div className="dropdown">
+            <ul>
+              <li>Newest First</li>
+              <li>Oldest First</li>
+              <li>Highest Rated</li>
+              <li>Lowest Rated</li>
+            </ul>
+          </div>
+
+          {/* {
+          this.state.showMenu
+            ? (
+              <div className="menu">
+                <button>Newest First</button>
+                <button>Oldest First</button>
+                <button>Highest Rated</button>
+                <button>Lowest Rated</button>
+              </div>
+            )
+            : (
+              null
+            )
+      } */}
+        </div>
       </span>
     );
   }
