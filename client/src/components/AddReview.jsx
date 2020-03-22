1e82ccec01e803e35c1749f788cd1cd3e86e4ab8
Grant Belcher <gtbelch@gmail.com>
2020-03-22 05:42:13 -0700

    begins implementing interactive rating bar

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -1,19 +1,89 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 import '../styles/AddReview.styles.scss';
 
+// class AddReview extends React.Component {
+//   constructor() {
+//     super();
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Rating />
+//       </div>
+//     );
+//   }
+// }
+
 class AddReview extends React.Component {
-    constructor(){
-        super();
-        this.state;
+  // propTypes: {
+  //   disabled: React.PropTypes.bool
+  // }
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  getInitialState() {
+    return {
+      rating: this.props.rating || null,
+      temp_rating: null,
+    };
+  }
+
+  rate(rating) {
+    this.setState({
+      rating,
+      temp_rating: rating,
+    });
+  }
+
+  star_over(rating) {
+    this.state.temp_rating = this.state.rating;
+    this.state.rating = rating;
+
+    this.setState({
+      rating: this.state.rating,
+      temp_rating: this.state.temp_rating,
+    });
+  }
+
+  star_out() {
+    this.state.rating = this.state.temp_rating;
+
+    this.setState({ rating: this.state.rating });
+  }
+
+  render() {
+    const stars = [];
+
+
+    for (let i = 0; i < 5; i++) {
+      let klass = 'star-rating__star  starRater';
+
+      if (this.state.rating >= i && this.state.rating != null) {
+        klass += ' is-selected starRater';
+      }
+
+      stars.push(
+        <span
+          onClick={this.rate.bind(this, i)}
+          onMouseOver={this.star_over.bind(this, i)}
+          onMouseOut={this.star_out}
+        >
+          <FontAwesomeIcon className={klass} icon={faStar} data-test="full" />
+        </span>,
+      );
     }
 
-    render() {
-        return (
-        <div className="reviewContainer">
-            <hr></hr>
-        </div>
-        )
-    }
+    return (
+      <div className="star-rating">
+        {stars}
+      </div>
+    );
+  }
 }
 
 export default AddReview;
