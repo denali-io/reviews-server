9f7118c0b277eeb629b2bfd352b70016e9627d26
Grant Belcher <gtbelch@gmail.com>
2020-03-22 18:09:23 -0700

    adds Share Icons to hover-links

--- client/src/components/Review/HoverLinks.jsx
+++ client/src/components/Review/HoverLinks.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight, faShareSquare } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/HoverLinks.styles.scss';
 
 function HoverLinks(props) {
   if (props.isHovered) {
     return (
-      <div >
-        <a className="shareLink">share review</a>
-        <a className="embedLink">embed review</a>
+      <div>
+        <a className="shareLink">
+          <FontAwesomeIcon icon={faShareSquare} className="hoverIcon" />
+          Share review
+        </a>
+        <a className="embedLink">
+          <FontAwesomeIcon icon={faChevronLeft} />
+          <FontAwesomeIcon icon={faChevronRight} className="hoverIcon" />
+          Embed review
+        </a>
       </div>
     );
   }
