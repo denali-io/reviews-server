5cb648af4aa7e377b329b5322e6c16323a8b1e46
Grant Belcher <gtbelch@gmail.com>
2020-03-22 18:34:11 -0700

    creates Modal Component that will render when hover link is clicked

--- client/src/components/Review/HoverLinks.jsx
+++ client/src/components/Review/HoverLinks.jsx
@@ -3,23 +3,60 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight, faShareSquare } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/HoverLinks.styles.scss';
 
-function HoverLinks(props) {
-  if (props.isHovered) {
-    return (
-      <div>
-        <a className="shareLink">
-          <FontAwesomeIcon icon={faShareSquare} className="hoverIcon" />
-          Share review
-        </a>
-        <a className="embedLink">
-          <FontAwesomeIcon icon={faChevronLeft} />
-          <FontAwesomeIcon icon={faChevronRight} className="hoverIcon" />
-          Embed review
-        </a>
-      </div>
-    );
+class HoverLinks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showShare: false,
+      showEmbed: false,
+    };
+    this.showShareModal = this.showShareModal.bind(this);
+    this.hideShareModal = this.hideShareModal.bind(this);
+    this.showEmbedModal = this.showEmbedModal.bind(this);
+    this.hideEmbedModal = this.hideEmbedModal.bind(this);
   }
-  return null;
-}
 
+  showShareModal() {
+    this.setState({
+      showShare: true,
+    });
+  }
+
+  hideShareModal() {
+    this.setState({
+      showShare: false,
+    });
+  }
+
+  showEmbedModal() {
+    this.setState({
+      showEmbed: true,
+    });
+  }
+
+  hideEmbedModal() {
+    this.setState({
+      showEmbed: false,
+    });
+  }
+
+  render() {
+    if (this.props.isHovered) {
+      return (
+        <div>
+          <a className="shareLink" onClick={this.showShareModal}>
+            <FontAwesomeIcon icon={faShareSquare} className="hoverIcon" />
+            Share review
+          </a>
+          <a className="embedLink">
+            <FontAwesomeIcon icon={faChevronLeft} />
+            <FontAwesomeIcon icon={faChevronRight} className="hoverIcon" />
+            Embed review
+          </a>
+        </div>
+      );
+    }
+    return null;
+  }
+}
 export default HoverLinks;
